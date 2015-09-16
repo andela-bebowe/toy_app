5d0afd4492ba83a1bac5c9da480bfb32d2ddc337
Ebowe Blessing <blessing.ebowe@andela.com>
2015-09-16 15:00:23 +0100

    Style edits

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -32,7 +32,8 @@ class MicropostsController < ApplicationController
         format.json { render :show, status: :created, location: @micropost }
       else
         format.html { render :new }
-        format.json { render json: @micropost.errors, status: :unprocessable_entity }
+        format.json { render json: @micropost.errors,
+                      status: :unprocessable_entity }
       end
     end
   end
@@ -46,7 +47,8 @@ class MicropostsController < ApplicationController
         format.json { render :show, status: :ok, location: @micropost }
       else
         format.html { render :edit }
-        format.json { render json: @micropost.errors, status: :unprocessable_entity }
+        format.json { render json: @micropost.errors,
+                      status: :unprocessable_entity }
       end
     end
   end
@@ -56,19 +58,22 @@ class MicropostsController < ApplicationController
   def destroy
     @micropost.destroy
     respond_to do |format|
-      format.html { redirect_to microposts_url, notice: 'Micropost was successfully destroyed.' }
+      format.html { redirect_to microposts_url, 
+                    notice: "Micropost was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_micropost
-      @micropost = Micropost.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def micropost_params
-      params.require(:micropost).permit(:content, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_micropost
+    @micropost = Micropost.find(params[:id])
+  end
+
+  # Never trust parameters from the
+  # scary internet, only allow the white list through.
+  def micropost_params
+    params.require(:micropost).permit(:content, :user_id)
+  end
 end
